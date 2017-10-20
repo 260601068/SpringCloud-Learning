8157840302122e1e24f7144c7da98a4e7e392cc8
260601068 <260601068@qq.com>
2017-10-20 17:11:36 +0800

    add myconsumer

---
+++ myconsumer/src/main/java/com/com/myconsumer/ConsumerController.java
@@ -0,0 +1,17 @@
+package com.com.myconsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+	@Autowired
+	HelloService helloService;
+	
+	@RequestMapping("ribbon-consumer")
+	public String helloConsumer(){
+		return helloService.helloService();
+	}
+}
